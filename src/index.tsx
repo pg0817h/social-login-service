89e07f66720874fd529e58a502ddc1698d02d6f3
Junsuk Park <park64kr63@gmail.com>
2023-03-03 15:35:29 +0900

    fix error

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import { BrowserRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 
 import App from '@/App';
-import '@/assets/scss/style.scss';
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
