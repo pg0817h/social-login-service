26b3f06b025521b501a181e966a53de40757fc2a
Geonhyeong Chun <pg0817h@gmail.com>
2023-03-15 21:16:27 -0700

    feat: Implement social login service

--- src/services/SocialLoginService.ts
+++ src/services/SocialLoginService.ts
@@ -1,3 +1,6 @@
+import KakaoLogin from './KakaoLogin';
+import NaverLogin from './NaverLogin';
+
 export type SocialLoginProviders = 'kakao' | 'naver';
 
 interface User {
@@ -25,7 +28,50 @@ class SocialLoginService {
   }
 
   async login(): Promise<LoginErrors | SocialLoginResponse> {
-    return LoginErrors.LoginProviderError;
+    const provider = this.provider;
+    switch (provider) {
+      case 'kakao': {
+        const kakaoLogin = new KakaoLogin();
+        try{
+          const response = await kakaoLogin.login();
+          const {provider_id, profile: {user :{id,email}, scopes}} = response
+  
+          const result: SocialLoginResponse = {
+            providerId: provider_id,
+            provider: this.provider,
+            user: { id: id },
+            scopes: scopes,
+            email: email,
+          };
+          return result;
+        } catch(error){
+          return LoginErrors.LoginProviderError;
+        }
+       
+      }
+      case 'naver': {
+        const naverLogin = new NaverLogin();
+        
+        try {
+          const response = await naverLogin.login();
+          const {provider: {id}, profile: {user_name, scopes,email}} = response
+          const result: SocialLoginResponse = {
+            providerId: id,
+            provider: this.provider,
+            user: { id: user_name },
+            scopes: scopes,
+            email: email,
+          };
+          return result;
+        
+        } catch(error){
+          return LoginErrors.LoginProviderError;
+        }
+        
+      } 
+      default:
+        return LoginErrors.LoginProviderError;
+    }
   }
 }
 
